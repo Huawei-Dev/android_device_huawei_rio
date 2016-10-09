de982e53f056f1431354aba5005ca640727fa944
Surdu Petru <surdu_petru@yahoo.com>
2016-10-09 16:15:09 +0200

    fix SignalStrength

--- ril/telephony/java/com/android/internal/telephony/HwQualcommRIL.java
+++ ril/telephony/java/com/android/internal/telephony/HwQualcommRIL.java
@@ -32,10 +32,17 @@ import android.telephony.SignalStrength;
  *
  * {@hide}
  */
-public class HwQualcommRIL extends RIL {
+public class HwQualcommRIL extends RIL implements CommandsInterface {
+
+    public HwQualcommRIL(Context context, int networkMode, int cdmaSubscription) {
+        this(context, networkMode, cdmaSubscription, null);
+    }
+
     public HwQualcommRIL(Context context, int networkMode, int cdmaSubscription, Integer instanceId) {
         super(context, networkMode, cdmaSubscription, instanceId);
     }
+
+    private static final String RILJ_LOG_TAG = "RILJ-HwQualcommRil";
 
     @Override
     protected Object
@@ -60,7 +67,7 @@ public class HwQualcommRIL extends RIL {
     protected void
     send(RILRequest rr) {
         if (rr.mRequest >= 132) {
-            Rlog.i(RILJ_LOG_TAG, "HwQualcommRil: Unsupported request " + rr.mRequest);
+            Rlog.i(RILJ_LOG_TAG, ": Unsupported request " + rr.mRequest);
             rr.onError(REQUEST_NOT_SUPPORTED, null);
             rr.release();
         } else {
@@ -71,25 +78,135 @@ public class HwQualcommRIL extends RIL {
     @Override
     protected Object
     responseSignalStrength(Parcel p) {
-        int gsmSignalStrength = p.readInt();
-        int gsmBitErrorRate = p.readInt();
-        int wcdmaRscp = p.readInt();
-        int wcdmaEcio = p.readInt();
-        int cdmaDbm = p.readInt();
-        int cdmaEcio = p.readInt();
-        int evdoDbm = p.readInt();
-        int evdoEcio = p.readInt();
-        int evdoSnr = p.readInt();
-        int lteSignalStrength = p.readInt();
-        int lteRsrp = p.readInt();
-        int lteRsrq = p.readInt();
-        int lteRssnr = p.readInt();
-        int lteCqi = p.readInt();
 
-        return new SignalStrength(gsmSignalStrength, gsmBitErrorRate,
-                        cdmaDbm, cdmaEcio,
-                        evdoDbm, evdoEcio, evdoSnr,
-                        lteSignalStrength, lteRsrp, lteRsrq, lteRssnr, lteCqi,
-                        wcdmaRscp, true);
+        Rlog.e(RILJ_LOG_TAG, "responseSignalStrength(): data size = " + p.dataSize());
+
+        int[] response = new int[16];
+        for (int i = 0 ; i < 16 ; i++) {
+            response[i] = p.readInt();
+        }
+
+        int gsmSignalStrength = response[0]; // Valid values are (0-31, 99) as defined in TS 27.007 8.5
+        int gsmBitErrorRate = response[1]; // bit error rate (0-7, 99) as defined in TS 27.007 8.5
+        int mWcdmaRscp = response[2]; // added by huawei
+        int mWcdmaEcio = response[3]; // added by huawei
+        int cdmaDbm = response[4];
+        int cdmaEcio = response[5];
+        int evdoDbm = response[6]; // -75 to -105, 99
+        int evdoEcio = response[7];
+        int evdoSnr = response[8]; // Valid values are 0-8.  8 is the highest signal to noise ratio
+        int lteSignalStrength = response[9]; // 0 to 12, 63
+        int lteRsrp = response[10]; // -85 to -140, -44
+        int lteRsrq = response[11]; // -3 to -20
+        int lteRssnr = response[12]; // 130 to -30, -200
+        int lteCqi = response[13];
+        int mGsm = response[14];
+        int mRat = response[15]; // added by huawei       
+
+        Rlog.d(RILJ_LOG_TAG, "---------- HEX ----------");
+        Rlog.d(RILJ_LOG_TAG, "gsmSignalStrength:" + String.format("%x", gsmSignalStrength));
+        Rlog.d(RILJ_LOG_TAG, "gsmBitErrorRate:" + String.format("%x", gsmBitErrorRate));
+        Rlog.d(RILJ_LOG_TAG, "mWcdmaRscp:" + String.format("%x", mWcdmaRscp));
+        Rlog.d(RILJ_LOG_TAG, "mWcdmaEcio:" + String.format("%x", mWcdmaEcio));
+        Rlog.d(RILJ_LOG_TAG, "cdmaDbm:" + String.format("%x", cdmaDbm));
+        Rlog.d(RILJ_LOG_TAG, "cdmaEcio:" + String.format("%x", cdmaEcio));
+        Rlog.d(RILJ_LOG_TAG, "evdoDbm:" + String.format("%x", evdoDbm));
+        Rlog.d(RILJ_LOG_TAG, "evdoEcio:" + String.format("%x", evdoEcio));
+        Rlog.d(RILJ_LOG_TAG, "evdoSnr:" + String.format("%x", evdoSnr));
+        Rlog.d(RILJ_LOG_TAG, "lteSignalStrength:" + String.format("%x", lteSignalStrength));
+        Rlog.d(RILJ_LOG_TAG, "lteRsrp:" + String.format("%x", lteRsrp));
+        Rlog.d(RILJ_LOG_TAG, "lteRsrq:" + String.format("%x", lteRsrq));
+        Rlog.d(RILJ_LOG_TAG, "lteRssnr:" + String.format("%x", lteRssnr));
+        Rlog.d(RILJ_LOG_TAG, "lteCqi:" + String.format("%x", lteCqi));
+        Rlog.d(RILJ_LOG_TAG, "mGsm:" + String.format("%x", mGsm));
+        Rlog.d(RILJ_LOG_TAG, "mRat:" + String.format("%x", mRat));
+
+        if (lteRsrp != 0) // LTE
+        {
+            if (lteRsrp > -20) lteSignalStrength = 64; // None or Unknown
+            else if (lteRsrp >= -97) lteSignalStrength = 63; // Great
+            else if (lteRsrp >= -105) lteSignalStrength = 11; // Good
+            else if (lteRsrp >= -113) lteSignalStrength = 7; // Moderate
+            else if (lteRsrp >= -120) lteSignalStrength = 4; // Poor
+            else if (lteRsrp >= -140) lteSignalStrength = 64; // None or Unknown
+        }
+        else if (gsmSignalStrength == 0 && lteRsrp == 0) // 3G
+        {  
+            lteRsrp = (mWcdmaRscp & 0xFF) - 256;
+            lteRsrq = (mWcdmaEcio & 0xFF) - 256;
+
+            if (lteRsrp > -20) { // None or Unknown
+                lteSignalStrength = 64;
+                lteRssnr = -200;
+            } else if (lteRsrp >= -85) { // Great
+                lteSignalStrength = 63;
+                lteRssnr = 300;
+            } else if (lteRsrp >= -95) { // Good
+                lteSignalStrength = 11;
+                lteRssnr = 129;
+            } else if (lteRsrp >= -105) { // Moderate
+                lteSignalStrength = 7;
+                lteRssnr = 44;
+            } else if (lteRsrp >= -115) { // Poor
+                lteSignalStrength = 4;
+                lteRssnr = 9;
+            } else if (lteRsrp >= -140) { // None or Unknown
+                lteSignalStrength = 64;
+                lteRssnr = -200;
+            }
+        }
+        else if (mWcdmaRscp == 0 && lteRsrp == 0) // 2G
+        {         
+            lteRsrp = (gsmSignalStrength & 0xFF) - 256;
+
+            if (lteRsrp > -20) { // None or Unknown
+                lteSignalStrength = 64;
+                lteRsrq = -21;
+                lteRssnr = -200;
+            } else if (lteRsrp >= -85) { // Great
+                lteSignalStrength = 63;
+                lteRsrq = -3;
+                lteRssnr = 300;
+            } else if (lteRsrp >= -95) { // Good
+                lteSignalStrength = 11;
+                lteRsrq = -7;
+                lteRssnr = 129;
+            } else if (lteRsrp >= -105) { // Moderate
+                lteSignalStrength = 7;
+                lteRsrq = -12;
+                lteRssnr = 44;
+            } else if (lteRsrp >= -115) { // Poor
+                lteSignalStrength = 4;
+                lteRsrq = -17;
+                lteRssnr = 9;
+            } else if (lteRsrp >= -140) { // None or Unknown
+                lteSignalStrength = 64;
+                lteRsrq = -21;
+                lteRssnr = -200;
+            }
+        }
+
+        gsmSignalStrength = 0;
+        gsmBitErrorRate = 0;
+        cdmaDbm = -1;
+        cdmaEcio = -1;
+        evdoDbm = -1;
+        evdoEcio = -1;
+        evdoSnr = -1;
+
+        Rlog.d(RILJ_LOG_TAG, "---------- MOD ----------");
+        Rlog.d(RILJ_LOG_TAG, "lteSignalStrength:" + lteSignalStrength);
+        Rlog.d(RILJ_LOG_TAG, "lteRsrp:" + lteRsrp);
+        Rlog.d(RILJ_LOG_TAG, "lteRsrq:" + lteRsrq);
+        Rlog.d(RILJ_LOG_TAG, "lteRssnr:" + lteRssnr);
+        Rlog.d(RILJ_LOG_TAG, "lteCqi:" + lteCqi);
+        Rlog.d(RILJ_LOG_TAG, "-------------------------");
+
+        SignalStrength signalStrength = new SignalStrength(
+            gsmSignalStrength, gsmBitErrorRate, cdmaDbm, cdmaEcio, evdoDbm, 
+            evdoEcio, evdoSnr, lteSignalStrength, -lteRsrp, -lteRsrq, 
+            lteRssnr, lteCqi, true);
+
+        return signalStrength;
     }
 }
